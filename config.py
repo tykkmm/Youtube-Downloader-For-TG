9547f3633da0d7f74a7dd895cf9d52aa50a01ca5
tykkmm <tykhanew45@gmail.com>
2024-07-25 06:34:34 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 import  os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-APP_ID = int(os.environ.get("API_ID"))
-API_HASH = os.environ.get("API_HASH")
-G1N = os.environ.get("GROUP_1_NAME")
-G2N = os.environ.get("GROUP_2_NAME")
-G1L = os.environ.get("GROUP_1_LINK")
-G2L = os.environ.get("GROUP_2_LINK")
-SPIC = os.environ.get("START_PIC")
-HPIC = os.environ.get("HELP_PIC")
+BOT_TOKEN = os.environ.get("7355006985:AAEY8ijg4CP-8GgcliRGJja87Tby78QT7To")
+APP_ID = int(os.environ.get("29426486"))
+API_HASH = os.environ.get("d71ad4ec048ab41677a1a439b21ff0c9")
+G1N = os.environ.get("Test V1")
+G2N = os.environ.get("Test V2")
+G1L = os.environ.get("https://t.me/+Z6Xn2T_qAQoxZTY0")
+G2L = os.environ.get("https://t.me/+adTciQD9qeEyODA0")
+SPIC = os.environ.get("https://mallucampaign.in/images/img_1718530865.jpg")
+HPIC = os.environ.get("https://mallucampaign.in/images/img_1718710206.jpg")
 youtube_next_fetch = 0 
 EDIT_TIME = 5
 
